8988f2efe0813dc27a5bcde627e86f8c906fc766
artechinteriors2012-blip <artechinteriors2012@gmail.com>
2025-08-27 17:07:26 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,321 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-kins-entrnce",
+      "name": "KINS ENTRNCE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3446888170112152
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5384585897599141,
+          "pitch": 0.07610767135604846,
+          "rotation": 3.141592653589793,
+          "target": "1-kins-hall"
+        },
+        {
+          "yaw": -0.06065732739336127,
+          "pitch": 0.028704087594642047,
+          "rotation": 10.995574287564278,
+          "target": "6-kins-gust-room"
+        },
+        {
+          "yaw": 0.261864448432652,
+          "pitch": 0.02130554913165028,
+          "rotation": 0,
+          "target": "4-kins-kitchen"
+        },
+        {
+          "yaw": 0.4671378636079986,
+          "pitch": 0.05853067229299569,
+          "rotation": 1.5707963267948966,
+          "target": "3-kins-dinning"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-kins-hall",
+      "name": "KINS HALL",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.0000026750999992941615,
+        "pitch": 0,
+        "fov": 1.3446888170112152
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.594885824589486,
+          "pitch": 0.061101198183401095,
+          "rotation": 7.853981633974483,
+          "target": "0-kins-entrnce"
+        },
+        {
+          "yaw": 0.8425299451856532,
+          "pitch": -0.010538578868333204,
+          "rotation": 4.71238898038469,
+          "target": "4-kins-kitchen"
+        },
+        {
+          "yaw": 1.3204284294808843,
+          "pitch": 0.07440862255157299,
+          "rotation": 3.141592653589793,
+          "target": "3-kins-dinning"
+        },
+        {
+          "yaw": 2.1525612274137425,
+          "pitch": 0.043543698027914246,
+          "rotation": 1.5707963267948966,
+          "target": "2-kins-master-room"
+        },
+        {
+          "yaw": 0.4290378111656139,
+          "pitch": 0.04309916832647431,
+          "rotation": 4.71238898038469,
+          "target": "6-kins-gust-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-kins-master-room",
+      "name": "KINS MASTER ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.2121246321654695,
+        "pitch": 0.026256127528807127,
+        "fov": 1.3446888170112152
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8529883385619677,
+          "pitch": 0.11416186847872822,
+          "rotation": 4.71238898038469,
+          "target": "1-kins-hall"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-kins-dinning",
+      "name": "KINS DINNING",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.5540991710877794,
+        "pitch": -0.06855571128981275,
+        "fov": 1.3446888170112152
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6700852633183114,
+          "pitch": -0.011371029218125273,
+          "rotation": 3.141592653589793,
+          "target": "4-kins-kitchen"
+        },
+        {
+          "yaw": -2.373862753070096,
+          "pitch": -0.016719017831190897,
+          "rotation": 4.71238898038469,
+          "target": "6-kins-gust-room"
+        },
+        {
+          "yaw": -2.970788884846339,
+          "pitch": 0.019052386297385482,
+          "rotation": 3.141592653589793,
+          "target": "1-kins-hall"
+        },
+        {
+          "yaw": 2.0953002298627466,
+          "pitch": 0.018715984214694004,
+          "rotation": 4.71238898038469,
+          "target": "2-kins-master-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-kins-kitchen",
+      "name": "KINS KITCHEN",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.034327814661779144,
+        "pitch": -0.016933942505648503,
+        "fov": 1.3446888170112152
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.929578527565255,
+          "pitch": 0.0362812095677949,
+          "rotation": 3.141592653589793,
+          "target": "3-kins-dinning"
+        },
+        {
+          "yaw": -2.65305197617497,
+          "pitch": -0.01895461204599691,
+          "rotation": 7.853981633974483,
+          "target": "5-kins-kids-room"
+        },
+        {
+          "yaw": -3.058622950430415,
+          "pitch": -0.008354612800618,
+          "rotation": 4.71238898038469,
+          "target": "2-kins-master-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-kins-kids-room",
+      "name": "KINS KIDS ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.6056006110190388,
+        "pitch": 0.23340674407104878,
+        "fov": 1.3446888170112152
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9723281549729315,
+          "pitch": 0.19229753204580646,
+          "rotation": 4.71238898038469,
+          "target": "1-kins-hall"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-kins-gust-room",
+      "name": "KINS GUST ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.11150174026725068,
+        "pitch": 0.12931305440713814,
+        "fov": 1.3446888170112152
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.617693448448332,
+          "pitch": 0.1400488368839632,
+          "rotation": 0,
+          "target": "3-kins-dinning"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
